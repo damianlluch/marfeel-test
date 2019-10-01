fcb049a48b77a58c0f725e29c79cda8cfdbcad8f
Damian Lluch <damianclluch@gmail.com>
2019-10-01 16:02:03 +0200

    upload

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ var htmlView = document.querySelector('#root');
 (async function () {
     var min = Math.ceil(1), max = Math.floor(3);
     var random = Math.floor(Math.random() * (max - min + 1)) + min;
-    const dataSet = await d3.json('/mock/mock' + random + '.json')
+    const dataSet = await d3.json('/mock/mock3.json')
 
     dataSet.forEach((data) => {
         var el = document.createElement('div');
