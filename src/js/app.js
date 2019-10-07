a68080186bba659d9d81703d6df6f1f314f593a7
Damian Lluch <damianclluch@gmail.com>
2019-10-07 12:26:40 +0200

    .

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,6 @@ import DataView from './dataView.js';
 var htmlView = document.querySelector('#root');
 
 (async function () {
-    var min = Math.ceil(1), max = Math.floor(3);
-    var random = Math.floor(Math.random() * (max - min + 1)) + min;
     const dataSet = await d3.json('/mock/mock3.json')
 
     dataSet.forEach((data) => {
